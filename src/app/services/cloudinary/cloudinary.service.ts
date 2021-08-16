6b395eb8aa7af650e9bd734b631b6c7773f58026
devlitus <developercarles@gmail.com>
2021-08-16 10:53:15 +0200

    implimentar cloudinary

---
+++ src/app/services/cloudinary/cloudinary.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { ImageCloudinary } from '../../models/imageCloudinary.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CloudinaryService {
+  private url: string =
+    'https://api.cloudinary.com/v1_1/djhxmjnb4/image/upload';
+  private currenUser = JSON.parse(localStorage.getItem('user') || 'demo');
+  
+  constructor(private http: HttpClient) {}
+
+  uploadImage(image: File): Observable<string> {
+
+    const body = new FormData();
+      body.append('file', image);
+      body.append('folder', `kahoot/${this.currenUser.displayName}`);
+      body.append('upload_preset', 'um7mj5dg');
+      return this.http.post<ImageCloudinary>(this.url, body).pipe(
+        map((image) => image.secure_url)
+      )
+  }
+}
